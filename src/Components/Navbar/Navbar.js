fe2c9ea28c060614fab69709495bfb4891d083d7
Jatin Karla <jatinkarla8@gmail.com>
2021-10-27 00:59:58 +0530

    website work started

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,57 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Navbar/Navbar.css";
+import ewblogo from "../../Images/ewb logo.png"
 
 function Navbar() {
-
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
-        <div class="container-fluid">
-          <Link class="navbar-brand" to="/">
-            EWB IET DAVV CHAPTER
+      <nav class='navbar navbar-expand-lg navbar-dark navbar-custom'>
+        <div class='container-fluid'>
+          <Link class='navbar-brand' to='/'>
+            <div>
+                <img src={ewblogo} alt="ewb-iet logo"/>
+                <span>IET DAVV CHAPTER</span>
+            </div>
+            
           </Link>
           <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            class='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <span class="navbar-toggler-icon"></span>
+            <span class='navbar-toggler-icon'></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link class="nav-link" aria-current="page" to="/about">
-                  About
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/hkbs">
-                  HKBS
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/nab">
-                  NAB
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/ssg">
-                  SSG
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/cg">
-                  Career Guidance
-                </Link>
-              </li>
-              
-
-              {/* <li class='nav-item dropdown'>
+          <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
+            <li class='nav-item dropdown'>
                 <a
                   class='nav-link dropdown-toggle'
                   href='#'
@@ -60,65 +37,52 @@ function Navbar() {
                   data-bs-toggle='dropdown'
                   aria-expanded='false'
                 >
-                  Dropdown
+                  Projects
                 </a>
                 <ul class='dropdown-menu' aria-labelledby='navbarDropdown'>
                   <li>
                     <a class='dropdown-item' href='#'>
-                      Action
+                      Helen Keller Blind School
                     </a>
                   </li>
                   <li>
                     <a class='dropdown-item' href='#'>
-                      Another action
+                      National Association for the Blind
                     </a>
                   </li>
                   <li>
+                    <a class='dropdown-item' href='#'>
+                      Social Service Group
+                    </a>
+                  </li>
+                  <li>
+                    <a class='dropdown-item' href='#'>
+                      Career Guidance
+                    </a>
+                  </li>
+                  {/* <li>
                     <hr class='dropdown-divider'></hr>
                   </li>
                   <li>
                     <a class='dropdown-item' href='#'>
                       Something else here
                     </a>
-                  </li>
+                  </li> */}
                 </ul>
-              </li> */}
-              {/* <li class='nav-item'>
-                <a class='nav-link disabled'>Disabled</a>
-              </li> */}
+              </li>
+              <li class='nav-item'>
+                <Link class='nav-link' aria-current='page' to='/about'>
+                  About
+                </Link>
+              </li>
+              <li class='nav-item'>
+                <Link class='nav-link' aria-current='page' to='/about'>
+                  Contact
+                </Link>
+              </li>
+
+              
             </ul>
-            <form class="d-flex">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success btn-sm" type="submit">
-                <span class="material-icons">search</span>
-              </button>
-            </form>
-            {/* <ul> */}
-            <Link to={!user && "/signup"}>
-              <button
-                onClick={handleAuthentication}
-                id="signup"
-                class="btn btn-outline-danger "
-              >
-                {user ? (
-                  <div>
-                    Sign Out <span class="st material-icons">logout</span>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                  </div>
-                ) : (
-                  <div>
-                    Sign Up <span class="st material-icons ">login</span>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                  </div>
-                )}
-              </button>
-            </Link>
-            {/* </ul> */}
           </div>
         </div>
       </nav>

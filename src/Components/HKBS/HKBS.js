b5f2e0ff1c81ae34de2afe7a83eaeef38eda630e
Jatin Karla <jatinkarla8@gmail.com>
2021-10-28 20:13:36 +0530

    i have explained the work to juniors

--- src/Components/HKBS/HKBS.js
+++ src/Components/HKBS/HKBS.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import "../HKBS/HKBS.css";
 
 function HKBS() {
-    return (
-        <div>
-            <h1>HKBS</h1>
-        </div>
-    )
+  return (
+    <div className='hkbsmain'>
+      <div className='hkbsmainheading'>
+        <h1>Helen Keller Blind School</h1>
+      </div>
+      <div className='hkbsmaincontent'>
+        React is an open source javascript library for building user interfaces.
+        React is a project created and maintained by Facebook. React has more
+        than a 100 thousand stars on GitHub and a huge community behind it.
+        React has become increasingly popular among developers and is also one
+        of the most sought out skill-sets by companies right now.
+      </div>
+    </div>
+  );
 }
 
-export default HKBS
+export default HKBS;
